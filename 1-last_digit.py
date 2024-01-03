f37720c997ec934094ae6cfceac4241e8f947e3b
Felix Opuge <omondifelix702@gmail.com>
2024-01-03 10:02:34 -0800

    loop

---
+++ 1-last_digit.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+str1 = " and is greater than 5"
+str2 = " and is 0"
+str3 = " and is less than 6 and not 0"
+if number < 0:
+    last = number % -10
+else:
+    last = number % 10
+if last > 5:
+    print(f"Last digit of {number:d} is {last:d}" + str1)
+elif last == 0:
+    print(f"Last digit of {number:d} is {last:d}" + str2)
+else:
+    print(f"Last digit of {number:d} is {last:d}" + str3)
