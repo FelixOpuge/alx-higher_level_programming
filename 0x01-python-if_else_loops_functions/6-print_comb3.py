cd075da12d4654f89bde24981c0897d357e6094d
Felix Opuge <omondifelix702@gmail.com>
2024-01-03 11:19:44 -0800

    complex combo

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-for num in range(10):
-    for num1 in range(10):
-        if num1 > num and num != 8:
-            print(f"{num:d}{num1:d}", end=", ")
-        elif num == 8 and num1 == 9:
-            print(f"{num:d}{num1:d}")
+for num1 in range(10):
+    for num2 in range(10):
+        if num2 > num1 and num1 != 8:
+            print("{}{}".format(num1, num2), end=", ")
+        elif num1 == 8 and num2 == 9:
+            print("{}{}".format(num1, num2))
             break
