6ecda37b0da55358214041f5a8b35d625b1f25f1
Felix Opuge <omondifelix702@gmail.com>
2024-01-24 05:47:31 -0800

    class Node that defines a node of a singly linked list by

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -1,109 +1,67 @@
 #!/usr/bin/python3
-"""Defines the classes Node and SinglyLinkedList"""
-
-
 class Node:
-    """
-    Class that defines properties Node.
-
-    Attributes:
-        data: data field of node.
-    """
     def __init__(self, data, next_node=None):
-        """Creates new instances of node.
-
-        Args:
-            __data : data field of node.
-        """
+        """Defines a node of a singly linked list"""
         self.data = data
         self.next_node = next_node
 
     @property
     def data(self):
-        """Retrieves the data field instance.
-
-        Returns: the data field of a node.
-        """
+        """data getter"""
         return self.__data
 
     @data.setter
     def data(self, value):
-        """Propery setter for data.
-
-        Args:
-            value (int): data field of a node.
-
-        Raises:
-            TypeError: data must be an integer
-        """
-        if not isinstance(value, int):
+        """data setter"""
+        if type(value) != int:
             raise TypeError("data must be an integer")
         self.__data = value
 
     @property
     def next_node(self):
-        """Retrives the next_node instance.
-
-        Returns: The next_node instance.
-        """
+        """next_node getter"""
         return self.__next_node
 
     @next_node.setter
     def next_node(self, value):
-        """Property setter for Node.
-
-        Args:
-            value (None): next node of a Node.
-
-        Raises:
-            TypeError: next_node must be a Node object .
-        """
-        if value is not None and not isinstance(value, Node):
+        """next_node setter"""
+        if value is not None and type(value) is not Node:
             raise TypeError("next_node must be a Node object")
         self.__next_node = value
 
 
 class SinglyLinkedList:
-    """
-    Class that defines properties of SinglyLinkedList.
-
-    Attributes:
-        head: head of the SinglyLinkedList.
-    """
     def __init__(self):
-        """Creates new instances of SinglyLinkedList .
-
-        Args:
-            __head : head of the SinglyLinkedList .
-        """
+        """Defines a singly linked list"""
         self.__head = None
 
-    def __str__(self):
-        """Represents the class objects as a string.
-
-        Returns: The class object represented as a string.
-        """
-        temp_var = self.__head
-        print_node = []
-        while temp_var:
-            print_node.sort()
-            print_node.append(str(temp_var.data))
-            temp_var = temp_var.next_node
-
-        print_node.sort(key=int)
-        return ("\n".join(print_node))
-
     def sorted_insert(self, value):
-        """Inserts a new node at a given position.
+        new = Node(value)
+        tmp = self.__head
+        add_start = False
 
-        Args:
-            value: value.
-        """
-        if self.__head is None:
-            new_node = Node(value)
-            new_node.next_node = self.__head
-            self.__head = new_node
+        if not self.__head:
+            self.__head = new
+            new.next_node = None
         else:
-            new_node = Node(value)
-            new_node.data = value
-            new_node.next_node = self.__head
+            if value < self.__head.data:
+                add_start = True
+            while tmp.next_node and value > tmp.next_node.data\
+                    and not add_start:
+                tmp = tmp.next_node
+            if not add_start:
+                    new.next_node = tmp.next_node
+                    tmp.next_node = new
+            else:
+                new.next_node = tmp
+                self.__head = new
+            new.data = value
+
+    def __str__(self):
+        s = ""
+        current = self.__head
+
+        while current:
+            s += str(current.data) + '\n'
+            current = current.next_node
+        return s[: -1]
