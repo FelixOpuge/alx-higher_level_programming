d399e17501007f66ce00fc1648c511212dd71bcc
Felix Opuge <omondifelix702@gmail.com>
2024-01-30 04:12:44 -0800

    Debug

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-"""0-rectangle, built for Holberton project 0x08 task 0.
-"""
+"""Defines an empty class Rectangle"""
 
 
 class Rectangle:
-    """Empty class per task instructions, will be built upon in later tasks.
+    """Class that defines properties of rectangle.
     """
     pass
